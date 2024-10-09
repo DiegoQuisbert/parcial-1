a3ef558f94bfffdab61b1aac5865fcde1c699bbd
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-10-09 19:10:37 -0300

    se agregaron solo los nombres de las funciones en movieController.js y las rutas en movieRouter.js, las funciones no están escritas o definidas aun

---
+++ controllers/movieController.js
@@ -0,0 +1,32 @@
+const Movie = require('../controller/movieModel.js');
+const Task = require('../models/taskModel');
+const User = require('../models/userModel');
+
+const createMovie = async(req, res) =>{
+
+}
+
+const getMovies = async(req, res) =>{
+
+}
+
+const getMovieById = async(req, res) => {
+
+}
+
+const updateMovieById = async(req, res) => {
+    
+}
+
+const deleteMovieById = async(req, res) => {
+
+}
+
+//método de filtrado
+
+//método de búsqueda
+const searchMovie = async(req, res) => {
+
+}
+
+module.exports = {createMovie, getMovies, getMovieById, updateMovieById, deleteMovieById, /* función de filtrado */ searchMovie};
--- routes/movieRouter.js
+++ routes/movieRouter.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const router = express.Router();
+
+const {createMovie, getMovies, getMovieById, updateMovieById, deleteMovieById, /* función de filtrado */ searchMovie} = require('../controllers/movieController');
+
+router.post('/', createMovie);
+router.get('/', getMovies);
+router.get('/', getMovieById);
+router.put('/', updateMovieById);
+router.delete('/', deleteMovieById);
+//router.get('/', método de filtrado);
+router.get('/', searchMovie);
+
+module.exports = router;
