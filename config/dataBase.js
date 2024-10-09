72c131b14fc2fffc5f2078b942c8cdfb7d5ea863
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-10-08 22:34:59 -0300

    copié y pegué el package json para así tener todas las dependencias y demás agregadas, además se añadió la carpeta config con su db y el index

---
+++ config/dataBase.js
@@ -0,0 +1,14 @@
+const moongose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(process.env.URI_BD);
+const db = mongoose.connection;
+
+db.on('error', () => console.error('Error'));
+db.once('open', ()=>{
+    console.log('Conexión correcta');
+});
+
+module.exports = db;
