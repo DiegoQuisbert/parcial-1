4719876154766f11e045b29fc9c4cdb0662acf1f
Nicolas Rodriguez <nicolas.mrodriguez@davinci.edu.ar>
2024-10-11 16:11:53 -0300

    pude realizar modificaciones en el index.html para que se vea los datos requeridos ademas de crear los metodos en el movieController.js y desarrolle los datos de los json de peliculas y de users

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,32 +1,97 @@
-const Movie = require('../controller/movieModel.js');
-const Task = require('../models/taskModel');
-const User = require('../models/userModel');
+const Movie = require('../models/movieModel'); 
 
-const createMovie = async(req, res) =>{
-
-}
-
-const getMovies = async(req, res) =>{
-
-}
-
-const getMovieById = async(req, res) => {
-
-}
-
-const updateMovieById = async(req, res) => {
+const createMovie = async (req, res) => {
     
-}
+};
 
-const deleteMovieById = async(req, res) => {
+const getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener las películas', error });
+    }
+};
 
-}
+const getMovieById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findById(id);
 
-//método de filtrado
+        if (!movie) {
+            return res.status(404).json({ message: 'Película no encontrada' });
+        }
 
-//método de búsqueda
-const searchMovie = async(req, res) => {
+        res.status(200).json(movie);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener la película', error });
+    }
+};
 
-}
+const updateMovieById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedMovie = await Movie.findByIdAndUpdate(id, req.body, { new: true });
 
-module.exports = {createMovie, getMovies, getMovieById, updateMovieById, deleteMovieById, /* función de filtrado */ searchMovie};
+        if (!updatedMovie) {
+            return res.status(404).json({ message: 'Película no encontrada' });
+        }
+
+        res.status(200).json(updatedMovie);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la película', error });
+    }
+};
+
+const deleteMovieById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedMovie = await Movie.findByIdAndDelete(id);
+
+        if (!deletedMovie) {
+            return res.status(404).json({ message: 'Película no encontrada' });
+        }
+
+        res.status(200).json({ message: 'Película eliminada correctamente' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la película', error });
+    }
+};
+
+
+const filterMovies = async (req, res) => {
+    try {
+        const { year, genre } = req.query;
+
+        let filter = {};
+        if (year) filter.releaseYear = year;
+        if (genre) filter.genre = genre;
+
+        const movies = await Movie.find(filter);
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al filtrar las películas', error });
+    }
+};
+
+
+const searchMovie = async (req, res) => {
+    try {
+        const { name } = req.query;
+
+        const movies = await Movie.find({ name: { $regex: name, $options: 'i' } });
+        res.status(200).json(movies);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al buscar la película', error });
+    }
+};
+
+module.exports = {
+    createMovie,
+    getMovies,
+    getMovieById,
+    updateMovieById,
+    deleteMovieById,
+    filterMovies,
+    searchMovie
+};
