b2b9171a1c6b0fd2e724ae788ab2986076f19f26
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-10-13 16:52:23 -0300

    pequeño monitoreo de algunas funciones, realizo este commit por si las dudas jaja

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const userRouter = require('./userRouter');
-const taskRouter = require('./taskRouter');
+const reviewRouter = require('./reviewsRouter');
 const movieRouter = require('./movieRouter');
 const directorRouter = require('./directorRouter');
 
 
 function routerAPI(app){
     app.use('/api/users', userRouter);
-    app.use('/api/tasks', taskRouter);
+    app.use('/api/reviews', reviewRouter);
     app.use('/api/movies', movieRouter);
     app.use('/api/directors', directorRouter);
 }
