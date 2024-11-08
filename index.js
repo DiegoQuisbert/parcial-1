d7c0fd3e3a4f635dd7ac9ee92442a7bc95a50b1e
Diego Quisbert <diego.quisbert@davinci.edu.ar>
2024-11-08 10:48:52 -0300

    wtf un cambio, se añadió cors y su uso en el index.js para poder realizar acciones en el form de react

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routerAPI = require('./routes');
 const db = require('./config/dataBase.js'); 
+const cors = require('cors');
 require('dotenv').config();
 
 const port = process.env.PORT; 
@@ -8,6 +9,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static('public')); 
 
 
